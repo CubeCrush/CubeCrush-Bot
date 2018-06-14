834cadd6912af4965766bf49b70a3dc31e018ccf
Zade <33807566+Zadezter@users.noreply.github.com>
2018-06-14 18:31:34 +0800

    Updated to vIR-3.3

--- implade.js
+++ implade.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     client.user.setStatus('dnd');
-    client.user.setActivity('with Implactor = vIR-1.2.6.2', {type: 'PLAYING'});
+    client.user.setActivity('with Implactor = vIR-1.2.7', {type: 'PLAYING'});
 });
 
 client.on('message', async message => {
@@ -16,7 +16,6 @@ client.on('message', async message => {
       .addField('$help', 'ImpladeBot command list!')
       .addField('$find', 'Check our websites!')
       .addField('$dcinvite', 'Join our developer team on Discord server!')
-      .addField('$serverenemy', 'List of our current enemy!')
       .addField('$picture', 'Take your picture from your Discord profile!')
       .addField('$implactor', 'Implactor - Pocketmine-MP plugin!')
       .addField('$botinfo', 'See this bot about infomations!')
@@ -52,28 +51,17 @@ client.on('message', async message => {
       let botinfoembed = new Discord.RichEmbed()
       .setTitle('ImpladeBot - Info')
       .setDescription('All of these version and status is on our information!')
-      .addField('Version', 'IR-3.1')
-      .addField('This server is running on', 'Heroku')
+      .addField('Version', 'IR-3.3')
+      .addField('This server is running on', 'Heroku Services')
       .addField('Bot was created on', '6 May 2018')
       return message.channel.send(botinfoembed);
-    };
-
-    if (message.content === '$serverenemy') {
-      let serverenemyembed = new Discord.RichEmbed()
-      .setTitle('[ Enemy ]')
-      .setDescription('This is our enemies list were put in here!')
-      .addField('ZythronPE - Multi-games server', 'Active in 2017 - 2018')
-      .addField('29 May 2018 - present', 'BeckenPlayZMCPE as a creator of ZythronPE server are now enemies against with the leader of ImpladeDeveloped team and founder of ImpladeMC, it is Zadezter! Also, Rampage is also known as Elctrokid9182_YT or Elctro. He is rival of Zadezter. In August 2017, he raided MCGenotix for ZythronPE for some false reason, he said, "you are suck so stop taking our staff"')
-      .setColor('#808000')
-      .setFooter(`Requested by ${message.author.tag}`)
-      return message.channel.send(serverenemyembed);
     };
 
     if (message.content === '$github') {
       let githubembed = new Discord.RichEmbed()
       .setTitle('ImpladeDeveloped - Github Team')
       .addField('Click here to see our codes!', 'https://github.com/ImpladeDeveloped')
-      .setDescription('We made some source codes for PHP and JS. Check it our repositories now!')
+      .setDescription('We made some source codes for PHP and JS. Check it our repositories now! You guys know we`re little coders!')
       .setColor('#000000')
       return message.channel.send(githubembed);
     };
@@ -82,13 +70,13 @@ client.on('message', async message => {
       let dcinviteembed = new Discord.RichEmbed()
       .setTitle('ImpladeDeveloped - Discord Server Team')
       .addField('Click here to join!', 'https://discord.gg/V8UhE9b')
-      .setDescription('This is our developer team server which coding a Pocketmine-MP PHP plugin and Discord bot basic with JS code!')
+      .setDescription('This is our developer team server, which coding a Pocketmine-MP PHP plugin but still learn, and Discord bot basic with JS code!')
       return message.channel.send(dcinviteembed);
     };
     
    if (message.content === '$picture') {
      let pictureembed = new Discord.RichEmbed()
-     .setTitle('Heyo, this is your profile picture?')
+     .setTitle('Heyo, this is your profile picture? Looks cool.')
      .setColor('RANDOM')
      .setImage(message.author.avatarURL + "?size=2048") 
      .setFooter(`Requested by ${message.author.tag}`)
@@ -99,7 +87,7 @@ client.on('message', async message => {
       let imcembed = new Discord.RichEmbed()
       .setTitle('ImpladeMC - MC:BE Discord Server')
       .addField('Click here to join our MC:BE server!', 'https://discord.gg/Fn9et4b')
-      .setDescription('A some kind of faction mode! Better than ZythronPE!')
+      .setDescription('This is a faction server which having some cool gameplay! (Not confirmed because it wasn`t released yet)')
       return message.channel.send(imcembed);
     };   
     
@@ -107,7 +95,7 @@ client.on('message', async message => {
       let friendsembed = new Discord.RichEmbed()
       .setTitle('[ Friends on Implade ]')
       .setDescription('This is some of people who is Zadezter`s friends on the list!')
-      .addField('---=_=---', 'MrLukePotatoes, JustyGamer45, TheMaskGuy11, WolfKid20044, MangoTheDev, DarkPrinceHQ, xXLloydPlayZMCXx, YourAScrubMate, haKingdomsCraft, ItzFlqmee, apnazu, MrCubeCraft, DragonPlayzMC, xMatthewRagez11, NikosGamer666, xSentinql, XxMegaIS123xX, YassenTrick')
+      .addField('---=_=---', 'MrLukePotatoes, JustyGamer45, TheMaskGuy11, WolfKid20044, MangoTheDev, DarkPrinceHQ, xXLloydPlayZMCXx, YourAScrubMate, haKingdomsCraft, ItzFlqmee, apnazu, MrCubeCraft, NikosGamer666, xSentinql, XxMegaIS123xX, YassenTrick[Dev], BeckenPlayZMCPE[Little nemesis], MuralFive5, Rojanthebest123, CrazyGamer5314')
       return message.channel.send(friendsembed);
     };   
     
@@ -115,8 +103,7 @@ client.on('message', async message => {
       let enemiesembed = new Discord.RichEmbed()
       .setTitle('[ Enemies on Implade ]')
       .setDescription('This is some of people who is Zadezter`s victim or enemy on the list!')
-      .addField('A staff of ZythronPE, is currently in nonsense war against ImpladeMC!', 'BeckenPlayZMCPE, TheStormFlame, iRampage22, shawngillett, MuralFive5, DarkHyperCraft[Indo]')
-      .addField('Other enemies', 'hung, AlligatorChicken, Corey')
+      .addField('Other enemies', 'hung, AlligatorChicken, Corey, Rampage, DarkHyperCraft[Indo], TheStormFlame, shawngillett')
       return message.channel.send(enemiesembed);
     };
 });
