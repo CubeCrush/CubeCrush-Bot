b1ee35b11140da5fbf9e8045efdb6b831586b7b4
MrLukePotatoes <30947763+MrLukePotatoes@users.noreply.github.com>
2018-06-22 17:51:15 +0800

    Update cubecrush.js

--- cubecrush.js
+++ cubecrush.js
@@ -10,51 +10,51 @@ client.on('message', async message => {
     if (message.author.bot) return;
 
     if (message.content === '>help') {
-      let helpembed = new Discord.RichEmbed()
-      .setTitle('- CubeCrush Bot | Commands -')
-      .setDescription('Prefix [ > ]')
-      .addField('>avatar', 'Gets your avatar',' ')
-      .addField('>ip', 'Shows the IP and Port for CubeCrush')
-      .addField('>socialmedia', 'Show‛s CubeCrush Owner’s Social Media')
-      .addField('>botabout', 'Show about the Bot information!')
-      .addField('>discord', 'Gives you the CubeCrush Discord Server Link so You can share it')
-      .addField('>user', 'Check your user information!')
-      .setColor('RANDOM')
-      .setFooter(`Requested by ${message.author.tag}`)
-      return message.channel.send(helpembed);
+    let helpembed = new Discord.RichEmbed()
+    .setTitle('- CubeCrush Bot | Commands -')
+    .setDescription('Prefix [ > ]')
+    .addField('>avatar', 'Gets your avatar',' ')
+    .addField('>ip', 'Shows the IP and Port for CubeCrush')
+    .addField('>socialmedia', 'Show‛s CubeCrush Owner’s Social Media')
+    .addField('>botabout', 'Show about the Bot information!')
+    .addField('>discord', 'Gives you the CubeCrush Discord Server Link so You can share it')
+    .addField('>user', 'Check your user information!')
+    .setColor('RANDOM')
+    .setFooter(`Requested by ${message.author.tag}`)
+    return message.channel.send(helpembed);
     };
     
     if (message.content === '>avatar') {
-     let profileembed = new Discord.RichEmbed()
-     .setTitle('Your avatar is cool.')
-     .setDescription(' ')
-     .setColor('RANDOM')
-     .setImage(message.author.avatarURL + "?size=2048") 
-     .setFooter(`Requested by ${message.author.tag}`)
-     return message.channel.send(profileembed)
+    let profileembed = new Discord.RichEmbed()
+    .setTitle('Your avatar is cool.')
+    .setDescription(' ')
+    .setColor('RANDOM')
+    .setImage(message.author.avatarURL + "?size=2048") 
+    .setFooter(`Requested by ${message.author.tag}`)
+    return message.channel.send(profileembed)
     };
     
     if (message.content === '>ip') {
-     let ipembed = new Discord.RichEmbed()
-     .setTitle('CubeCrush Server IP&Port')
-     .setDescription('>help for More Commands')
-     .addField('IP - play.cubecrush.tk','Port - 19130')
-     .setColor('RANDOM')
-     .setFooter(` Requested by ${message.author.tag}`)
-     return message.channel.send(ipembed)
+    let ipembed = new Discord.RichEmbed()
+    .setTitle('CubeCrush Server IP&Port')
+    .setDescription('>help for More Commands')
+    .addField('IP - play.cubecrush.tk','Port - 19130')
+    .setColor('RANDOM')
+    .setFooter(` Requested by ${message.author.tag}`)
+    return message.channel.send(ipembed)
     };
     
     if (message.content === '>user') {
-     let userembed = new Discord.RichEmbed()
-     .setTitle(`${message.author.tag}'s User Information`)
-     .setColor('RANDOM')
-     .setThumbnail(client.user.avatarURL)
-     .addField('Your ID:', message.author.id, true)
-     .addField('Joined on:', message.member.joinedAt.toDateString(), true)
-     .addField('Account Type:', message.member.user.bot ? 'Bot' : 'Personal', true)
-     .setFooter(`Requested by ${message.author.tag}`)
+    let userembed = new Discord.RichEmbed()
+    .setTitle(`${message.author.tag}'s Info`)
+    .setColor('RANDOM')
+    .setThumbnail(client.user.avatarURL)
+    .addField('Your ID:', message.author.id, true)
+    .addField('Joined on:', message.member.joinedAt.toDateString(), true)
+    .addField('Account Type:', message.member.user.bot ? 'Bot' : 'Personal', true)
+    .setFooter(`Requested by ${message.author.tag}`)
     return message.channel.send(userembed);
-   };
+    };
     
     if (message.content === '>botabout') {
     let baembed = new Discord.RichEmbed()
